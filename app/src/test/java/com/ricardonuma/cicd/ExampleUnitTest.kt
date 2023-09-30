964b7e3f415b329771b3177480a1c6dab9bb2eca
Ricardo Numa <koki_numa@hotmail.com>
2023-09-29 22:02:17 -0700

    Add ktlintCheck to github actions and force ktlint fail

--- app/src/test/java/com/ricardonuma/cicd/ExampleUnitTest.kt
+++ app/src/test/java/com/ricardonuma/cicd/ExampleUnitTest.kt
@@ -12,4 +12,24 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addition_isCorrect2() {
+        assertEquals(2, 1 + 1)
+    }
+
+    @Test
+    fun addition_isCorrect3() {
+        assertEquals(3, 1 + 2)
+    }
+
+    @Test
+    fun addition_isCorrect4() {
+        assertEquals(5, 3 + 2)
+    }
+
+    @Test
+    fun addition_isCorrect5() {
+        assertEquals(6, 4 + 2)
+    }
 }
